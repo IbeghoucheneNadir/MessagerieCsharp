a69fcd4054305bb40a6a2ea1276b07a34bc4054b
boudradar <hamza.boudradar@etu.unice.fr>
2019-03-28 18:51:26 +0100

    Register Form and User

---
+++ Wpf1/Register.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf1
+{
+    /// <summary>
+    /// Logique d'interaction pour Register.xaml
+    /// </summary>
+    public partial class Register : UserControl
+    {
+        public Register()
+        {
+            InitializeComponent();
+        }
+
+        private void add(object sender, RoutedEventArgs e)
+        {
+            this.RemoveVisualChild(new Register());
+            this.AddChild(new Window1());
+            nom.Clear();
+            password.Clear();
+            password1.Clear();
+            
+        }
+    }
+}
